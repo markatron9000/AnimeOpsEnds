41a6c38f5bbafbad557737d57628a02c0b77a4d3
Mark Matis <markmatis11@live.com>
2021-04-07 14:48:41 +0000

    Write output to file, fix searching youtube with python

--- YoutubeMPThreeconvert.py
+++ YoutubeMPThreeconvert.py
@@ -5,6 +5,8 @@ Created on Tue Aug 25 18:03:55 2020
 @author: markm
 """
 
+#Not currently used, but may one day be implemented. It would return mp3's instead of url's. I
+#am unsure if I want to offer that functionality or not.
 from __future__ import unicode_literals
 import youtube_dl
 
